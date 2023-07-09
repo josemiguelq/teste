2387f0fff43a031c541c7332db3b68d0ed19a39a
Jose Miguel <jose.miguelq95@gmail.com>
2023-07-09 19:22:14 -0300

    added app

--- api/index.py
+++ api/index.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import request
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+
+@app.route("/webhook/", methods=["POST", "GET"])
+def webhook_whatsapp():
+    """__summary__: Get message from the webhook"""
+    vt = os.environ.get("VERIFY_TOKEN", default="true")
+
+    if request.method == "GET":
+        if request.args.get('hub.verify_token') == vt:
+            return request.args.get('hub.challenge')
+        return "Authentication failed. Invalid Token."
+
+    client = WhatsAppWrapper()
+
+    response = client.process_webhook_notification(request.get_json())
+
+    # Do anything with the response
+    # Sending a message to a phone number to confirm the webhook is working
+
+    return jsonify({"status": "success"}, 200)
 
 @app.route('/about')
 def about():

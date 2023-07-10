ef70bbfcfe9bca82dff73ab71d2ef81342f92111
Jose Miguel <jose.miguelq95@gmail.com>
2023-07-09 22:40:23 -0300

    wip

--- api/index.py
+++ api/index.py
@@ -28,7 +28,7 @@ def webhook_whatsapp():
 #    response = client.process_webhook_notification(request.get_json())
     my_headers = {'Authorization' : 'Bearer ' + wpp, 'Content-Type': 'application/json'}
     my_mes = { 'messaging_product': 'whatsapp', 'to': '5567991910048', "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }
-    response = requests.post('https://graph.facebook.com/v17.0/105496645940349/messages', headers=my_headers, data=my_mes)
+    response = requests.post('https://graph.facebook.com/v17.0/105496645940349/messages', headers=my_headers, json=my_mes)
     print(response.content)
 
     # Do anything with the response
